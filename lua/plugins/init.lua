32f2031fb60d7287e7958ecca4944971fde39779
maciejgrzes <maciej.grzes07@gmail.com>
2026-05-25 23:45:50 +0200

    added nvim-dap

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -51,4 +51,69 @@ return {
       return require "nvchad.configs.nvimtree"
     end,
   },
+
+  {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      "rcarriga/nvim-dap-ui",
+      "nvim-neotest/nvim-nio",
+    },
+    config = function()
+      local dap = require("dap")
+      local dapui = require("dapui")
+
+      dapui.setup()
+
+      dap.listeners.before.attach.dapui_config = function()
+        dapui.open()
+      end
+
+      dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+      end
+
+      dap.listeners.before.event_terminated.dapui_config = function()
+        dapui.close()
+      end
+
+      dap.listeners.before.event_exited.dapui_config = function()
+        dapui.close()
+      end
+
+      dap.adapters.codelldb = {
+        type = "server",
+        port = "${port}",
+        executable = {
+          command = vim.fn.stdpath("data") .. "/mason/bin/codelldb",
+          args = { "--port", "${port}" },
+        },
+      }
+
+      dap.configurations.cpp = {
+        {
+          name = "Launch executable",
+          type = "codelldb",
+          request = "launch",
+          program = function()
+            return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+          end,
+          cwd = "${workspaceFolder}",
+          stopOnEntry = false,
+        },
+      }
+
+      dap.configurations.c = dap.configurations.cpp
+      dap.configurations.rust = dap.configurations.cpp
+
+      local map = vim.keymap.set
+
+      map("n", "<leader>db", dap.toggle_breakpoint, { desc = "DAP toggle breakpoint" })
+      map("n", "<leader>dc", dap.continue, { desc = "DAP continue/start" })
+      map("n", "<leader>dn", dap.step_over, { desc = "DAP step over" })
+      map("n", "<leader>di", dap.step_into, { desc = "DAP step into" })
+      map("n", "<leader>do", dap.step_out, { desc = "DAP step out" })
+      map("n", "<leader>dr", dap.repl.open, { desc = "DAP REPL" })
+      map("n", "<leader>du", dapui.toggle, { desc = "DAP UI toggle" })
+    end,
+  },
 }
